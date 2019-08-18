5f6b2121dcd2f4d4a584edf022ad5eb3c12076a0
Uraswa <33593469+Uraswa@users.noreply.github.com>
2019-08-18 18:17:16 +0300

    Frontend app created

---
+++ frontend/client/src/index.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import * as serviceWorker from './serviceWorker';
+import ApolloProvider from "./ApolloProvider";
+
+ReactDOM.render(ApolloProvider, document.getElementById('root'));
+
+serviceWorker.unregister();
